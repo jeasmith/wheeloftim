1cc3fe64e7c682f5cd644ce6927d3663c482675a
Jamie Smith <jeasmith@me.com>
2024-04-07 22:13:22 +0200

    enable custom server for lighthouse

--- apps/web/lighthouserc.js
+++ apps/web/lighthouserc.js
@@ -1,5 +1,9 @@
 module.exports = {
   ci: {
+    collect: {
+      url: ['http://localhost:3000/'],
+      startServerCommand: 'bun run start',
+    },
     upload: {
       target: 'temporary-public-storage',
     },
