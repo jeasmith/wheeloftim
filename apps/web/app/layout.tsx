95fb9a181842716b605610151401a83197411d92
Jamie Smith <jeasmith@me.com>
2024-04-07 21:35:15 +0200

    Update layout.tsx (#40)
    * Update layout.tsx
    
    Add speed insights component
    
    Signed-off-by: Jamie Smith <jeasmith@me.com>
    
    * Update package.json
    
    Add dependency
    
    Signed-off-by: Jamie Smith <jeasmith@me.com>
    
    * Update package.json
    
    Fix bad config
    
    Signed-off-by: Jamie Smith <jeasmith@me.com>
    
    * Update layout.tsx
    
    fix bad import
    
    Signed-off-by: Jamie Smith <jeasmith@me.com>
    
    ---------
    
    Signed-off-by: Jamie Smith <jeasmith@me.com>

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
+      <SpeedInsights/>
       <body className={inter.className}>{children}</body>
     </html>
   );
